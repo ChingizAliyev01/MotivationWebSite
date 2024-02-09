e1d3e2d98406764e0fb4f3473c81d380a2846cc1
ChingizAliyev01 <chingizaliyev01@gmail.com>
2024-02-09 05:12:31 +0400

    Changed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,12 +8,12 @@ const Header = () => {
         <nav class="navbar navbar-expand-lg  ">
             <div class="container">
                 <RouterLink class="navbar-brand" to="/"><img class='logo' src="./assets/images/logo.png" alt="" /></RouterLink>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                {/* <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <FaBars className='fabars'/>
-                </button>
+                </button> */}
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <form class="d-flex ms-auto mb-2 mb-lg-0" role="search">
-                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                        <input class="form-control me-4" type="search" placeholder="Search" aria-label="Search" />
                         <button class="btn btn-outline-success" type="submit">Search</button>
                     </form>
                 </div>
